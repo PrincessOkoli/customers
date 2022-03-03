a3ea82ca6699070d5aef7af9b31354aabca67d42
PrincessOkoli <juskie115@gmail.com>
2022-03-02 15:49:22 -1200

    i corrected my error

--- customer_record/settings.py
+++ customer_record/settings.py
@@ -130,7 +130,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "statiC"),
 ]
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-# STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 from django.contrib.messages import constants as messages
 MESSAGE_TAGS = {
